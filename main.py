2490cb87de91d2fb8cc1efa637e5dc8322a0ed67
João Gabriel <91282960+COOSMOS42@users.noreply.github.com>
2024-11-18 16:19:42 -0300

    Update and rename testinho.py to main.py

---
+++ main.py
@@ -0,0 +1,13 @@
+import streamlit as st
+
+
+def paginainicial():
+    st.title("Início")
+
+pg = st.navigation([
+    st.Page(paginainicial, title="Página Inicial", icon="🪙"),
+    st.Page("controlador.py", title="Atualizar Status", icon="💾"),
+    st.Page("consultas.py", title="Consultar Documentos", icon="🗃️")
+
+])
+pg.run()
