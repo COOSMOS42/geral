5c6c48e282b9a15f43e90592732ce879fb7cd2d3
João Gabriel <91282960+COOSMOS42@users.noreply.github.com>
2024-11-19 17:21:06 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-with open("styles.css") as f:
+with open("stylesmain.css") as f:
     st.markdown(f"<style>{f.read()}<style>", unsafe_allow_html=True)
     
 def paginainicial():
