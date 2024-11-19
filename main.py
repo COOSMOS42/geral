8a155e9946d2a59b4b7b68028771f0c0c82b982f
João Gabriel <91282960+COOSMOS42@users.noreply.github.com>
2024-11-19 14:58:30 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
 import streamlit as st
 
+def paginainicial():
+    st.title("Second page")
+    
+
 
 pg = st.navigation([
+    st.Page(paginainicial, title="Página Inicial", icon="🪙"),
     st.Page("controlador.py", title="Atualizar Status", icon="💾"),
     st.Page("consultas.py", title="Consultar Documentos", icon="🗃️")
 
