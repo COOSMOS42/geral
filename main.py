82f1c0267934aff54f2bcdd4165142fb4d0e11f6
João Gabriel <91282960+COOSMOS42@users.noreply.github.com>
2024-11-19 17:19:19 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 import streamlit as st
 
+with open("styles.css") as f:
+    st.markdown(f"<style>{f.read()}<style>", unsafe_allow_html=True)
+    
 def paginainicial():
     st.write("Sistema de Acompanhamento")
 
