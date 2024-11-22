b1b2a94d216ab8a3f954a9c1c6ce57bfc21b24af
João Gabriel <91282960+COOSMOS42@users.noreply.github.com>
2024-11-22 14:08:10 -0300

    Update remover.py

--- remover.py
+++ remover.py
@@ -1,1 +1,43 @@
+import streamlit as st
+import pandas as pd
+from datetime import datetime, timedelta
+import gspread
+from google.oauth2.service_account import Credentials
+from google.oauth2 import service_account
+from gspread_pandas import Spread, Client
+from gspread_dataframe import set_with_dataframe
 
+# Definir escopos para Google Sheets e Google Drive
+scope = [
+    "https://spreadsheets.google.com/feeds",
+    "https://www.googleapis.com/auth/drive"
+]
+
+# Carregar as credenciais de acesso do arquivo JSON
+creds = service_account.Credentials.from_service_account_info(st.secrets["gcp_service_account"], scopes= scope)
+# Autenticar com o Google Sheets (conectar as credencias)
+client = Client(scope=scope, creds=creds)
+spreadsheetname = "controlador"
+spread = Spread(spreadsheetname, client = client)
+#link com a planilha do google sheets
+sheet = client.open(spreadsheetname).sheet1
+
+val = sheet.get_all_values()
+# fr é a variavel da planilha do google sheets
+fr = pd.DataFrame(val)
+#separa a primeira linha da planilha google sheets
+cab = fr.iloc[0]
+#fazendo com que a planilha seja lida a partir da primeira linha
+fr = fr[1:]
+#seta as colunas
+fr.columns = cab
+st.set_page_config(layout='wide')
+
+#formulário para remoção de linhas da planilha do google sheets
+    with st.form('rmv', clear_on_submit=True, border=True):
+        st.subheader('Remover entrega')
+        ind = st.number_input('Qual entrega deseja remover?', 1, 9999, 1)
+        ind2 = ind + 1
+
+        if st.form_submit_button('Remover'):
+            sheet.delete_rows(ind2)
