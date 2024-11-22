503d29db3936ed8207389c7d4ea14a4462d433a3
João Gabriel <91282960+COOSMOS42@users.noreply.github.com>
2024-11-22 14:16:59 -0300

    Update main.py

--- main.py
+++ main.py
@@ -3,6 +3,6 @@ import streamlit as st
 pg = st.navigation([
     st.Page("controlador.py", title="Atualizar Status", icon="💾"),
     st.Page("consultas.py", title="Consultar Documentos", icon="🗃️")
-
+    st.Page("remover.py", title="Remover Documentos", icon="🗑️")
 ])
 pg.run()
