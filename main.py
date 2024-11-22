069324e578d5acfb63b85da73269af62467ce05f
João Gabriel <91282960+COOSMOS42@users.noreply.github.com>
2024-11-22 13:39:39 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
 import streamlit as st
 
-
-def paginainicial():
-    st.write("Sistema de Acompanhamento")
-
 pg = st.navigation([
-    st.Page(paginainicial, title="Página Inicial", icon="🪙"),
     st.Page("controlador.py", title="Atualizar Status", icon="💾"),
     st.Page("consultas.py", title="Consultar Documentos", icon="🗃️")
 
