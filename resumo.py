1878e6c6daf77b7694cdce2fdf488e6c058773fb
João Gabriel <91282960+COOSMOS42@users.noreply.github.com>
2024-11-27 16:32:15 -0300

    Update resumo.py

--- resumo.py
+++ resumo.py
@@ -41,37 +41,37 @@ column_1, column_2, column_3, column_4, column_5, column_6 = st.columns(6)
 column_7, column_8, column_9, column_10, column_11, column_12 = st.columns(6)
 
 with column_1:
-    plot_gauge(8, "#0068C9", "", "PARCELA 01", 8)
+    plot_gauge(8, "#78FF0F", "", "PARCELA 01", 8)
 
 with column_2:
-    plot_gauge(8, "#FF8700", "", "PARCELA 02", 8)
+    plot_gauge(8, "#78FF0F", "", "PARCELA 02", 8)
 
 with column_3:
-    plot_gauge(8, "#FF2B2B", "", "PARCELA 03", 8)
+    plot_gauge(8, "#78FF0F", "", "PARCELA 03", 8)
 
 with column_4:
-    plot_gauge(7, "#29B09D", "", "PARCELA 04", 8)
+    plot_gauge(7, "#78FF0F", "", "PARCELA 04", 8)
 
 with column_5:
-    plot_gauge(0, "#BA6FF2", "", "PARCELA 05", 8)
+    plot_gauge(0, "#78FF0F", "", "PARCELA 05", 8)
 
 with column_6:
-    plot_gauge(0, "#F26FBC", "", "PARCELA 06", 8)
+    plot_gauge(0, "#78FF0F", "", "PARCELA 06", 8)
 
 with column_7:
-    plot_gauge(0, "#E56FF2", "", "PARCELA 07", 8)
+    plot_gauge(0, "#78FF0F", "", "PARCELA 07", 8)
 
 with column_8:
-    plot_gauge(0, "#906FF2", "", "PARCELA 08", 8)
+    plot_gauge(0, "#78FF0F", "", "PARCELA 08", 8)
 
 with column_9:
-    plot_gauge(0, "#F2706F", "", "PARCELA 09", 8)
+    plot_gauge(0, "#78FF0F", "", "PARCELA 09", 8)
 
 with column_10:
-    plot_gauge(0, "#EDC0F2", "", "PARCECLA 10", 8)
+    plot_gauge(0, "#78FF0F", "", "PARCECLA 10", 8)
 
 with column_11:
-    plot_gauge(0, "#49A356", "", "PARCELA 11", 8)
+    plot_gauge(0, "#78FF0F", "", "PARCELA 11", 8)
 
 with column_12:
-    plot_gauge(0, "#37844A", "", "PARCELA 12", 8)
+    plot_gauge(0, "#78FF0F", "", "PARCELA 12", 8)
