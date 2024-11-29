7c46e6117dd3508caa9e9c694ee6e3fe5f4694a5
João Gabriel <91282960+COOSMOS42@users.noreply.github.com>
2024-11-29 16:31:17 -0300

    Update resumo.py

--- resumo.py
+++ resumo.py
@@ -24,7 +24,7 @@ client = Client(scope=scope, creds=creds)
 spreadsheetname = "controlador"
 spread = Spread(spreadsheetname, client = client)
 #link com a planilha do google sheets
-sheet = client.open(spreadsheetname).sheet2
+sheet = client.open(spreadsheetname).sheet1
 
 val = sheet.get_all_values()
 # fr é a variavel da planilha do google sheets
