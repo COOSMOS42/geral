45ba353fe568051844358d039f083e457671ee8e
João Gabriel <91282960+COOSMOS42@users.noreply.github.com>
2024-12-03 16:46:16 -0300

    Update consultas.py

--- consultas.py
+++ consultas.py
@@ -51,7 +51,7 @@ with st.form('proc', clear_on_submit=True, border=True):
         my_bar = st.progress(0, text=progress_text)
     
         for percent_complete in range(100):
-            time.sleep(0.01)  # Pausa breve para efeito visual
+            time.sleep(0.01)
             my_bar.progress(percent_complete + 1, text=progress_text)
 
 
