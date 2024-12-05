4fb657270a5bba1d265e2c4d9562d7f98fbec1a4
João Gabriel <91282960+COOSMOS42@users.noreply.github.com>
2024-12-05 15:39:21 -0300

    Update glosas.py

--- glosas.py
+++ glosas.py
@@ -44,12 +44,13 @@ if 'jsoninput' not in st.session_state:
 
 #adiciona um entrega a lista acima
 # dia 07/11 foi alterado de Destinatário para Status pois não faz sentido manter como destinario se com status eu tenho mais informações
-def adicionar_entrega(Data, Parcela, Documento, Glosa):
+def adicionar_entrega(Data, Parcela, Documento, Glosa, Observação):
     entrega = {
         'data': Data,
         'parcela': Parcela,
         'documento': Documento,
         'glosa' : Glosa,
+        'observação' : Observação
     }
 
     st.session_state.jsoninput = pd.concat(
@@ -80,13 +81,16 @@ with st.form('Preencha os dados', clear_on_submit=True, border=True):
     pardoc = parcela + " " + documento
 
     st.subheader('Glosa')
-    obs = st.text_input('Qual o Valor da Glosa?')
+    glosa = st.text_input('Qual o Valor da Glosa?')
+
+    st.subheader('Observação')
+    obs = st.text_input('Alguma observação?')
 
     st.subheader('Adicionar Glosa')
 
     if st.form_submit_button('Adicionar'):
         st.session_state.jsoninput = adicionar_entrega(
-            dataformat, parcela, documento, obs)
+            dataformat, parcela, documento, glosa, obs)
 
 st.subheader('Salvar entregas')
 
