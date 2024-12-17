cbac4f6f52212f8ab1fb22b2ca02242acef33fd9
João Gabriel <91282960+COOSMOS42@users.noreply.github.com>
2024-12-17 14:50:44 -0300

    Update resumo.py

--- resumo.py
+++ resumo.py
@@ -25,6 +25,10 @@ spreadsheetname = "controlador"
 spread = Spread(spreadsheetname, client = client)
 #link com a planilha do google sheets
 sheet = client.open(spreadsheetname).worksheet("resumo")
+#sheet para os valores dos documentos aprovados
+sheet2 = client.open(spreadsheetname).worksheet("aprovado")
+
+
 
 val = sheet.get_all_values()
 # fr é a variavel da planilha do google sheets
@@ -35,6 +39,17 @@ cab = fr.iloc[0]
 fr = fr[1:]
 #seta as colunas
 fr.columns = cab
+
+#dataframe dos documentos aprovados
+val100 = sheet2.get_all_values()
+# fr é a variavel da planilha do google sheets
+fr1 = pd.DataFrame(val100)
+#separa a primeira linha da planilha google sheets
+cab1 = fr1.iloc[0]
+#fazendo com que a planilha seja lida a partir da primeira linha
+fr1 = fr1[1:]
+#seta as colunas
+fr1.columns = cab1
 
 def plot_gauge(
         indicator_number, indicator_color, indicator_suffix, indicator_title, max_bound
@@ -105,6 +120,47 @@ val11 = pd.to_numeric(val11, errors='coerce')
 val12 = fr["nentregas"].iloc[11] 
 val12 = pd.to_numeric(val12, errors='coerce')
 
+
+
+val13 = fr["nentregas"].iloc[0] 
+val13 = pd.to_numeric(val13, errors='coerce')
+
+val14 = fr["nentregas"].iloc[1] 
+val14 = pd.to_numeric(val14, errors='coerce')
+
+val15 = fr["nentregas"].iloc[2] 
+val15 = pd.to_numeric(val15, errors='coerce')
+
+val16 = fr["nentregas"].iloc[3] 
+val16 = pd.to_numeric(val16, errors='coerce')
+
+val17 = fr["nentregas"].iloc[4] 
+val17 = pd.to_numeric(val17, errors='coerce')
+
+val18 = fr["nentregas"].iloc[5] 
+val18 = pd.to_numeric(val18, errors='coerce')
+
+val19 = fr["nentregas"].iloc[6] 
+val19 = pd.to_numeric(val19, errors='coerce')
+
+val20 = fr["nentregas"].iloc[7] 
+val20 = pd.to_numeric(val20, errors='coerce')
+
+val21 = fr["nentregas"].iloc[8] 
+val21 = pd.to_numeric(val21, errors='coerce')
+
+val22 = fr["nentregas"].iloc[9] 
+val22 = pd.to_numeric(val22, errors='coerce')
+
+val23 = fr["nentregas"].iloc[10] 
+val23 = pd.to_numeric(val23, errors='coerce')
+
+val24 = fr["nentregas"].iloc[11] 
+val24 = pd.to_numeric(val24, errors='coerce')
+
+
+
+
 total_entregas = sum([val1, val2, val3, val4, val5, val6, val7, val8, val9, val10, val11, val12])
 total_max = 90
 progresso = (total_entregas / total_max) * 100 if total_max > 0 else 0
@@ -117,6 +173,10 @@ st.write(f"Progresso atual: {progresso:.2f}%")
 column_1, column_2, column_3, column_4, column_5, column_6 = st.columns(6)
 
 column_7, column_8, column_9, column_10, column_11, column_12 = st.columns(6)
+
+column_13, column_14, column_15, column_16, column_17, column_18 = st.columns(6)
+
+column_19, column_20, column_21, column_22, column_23, column_24 = st.columns(6)
 
 with column_1:
     plot_gauge(val1, "#1C83E1", "", "PARCELA 01", 8)
@@ -153,3 +213,57 @@ with column_11:
 
 with column_12:
     plot_gauge(val12, "#1C83E1", "", "PARCELA 12", 8)
+
+
+with st.expander('Mostrar Aprovados'):
+    with colum_13:
+        plot_gauge(val13, "#1C83E1", "", "PARCELA 1", 8)
+
+    with colum_14:
+        plot_gauge(val14, "#1C83E1", "", "PARCELA 2", 8)
+
+    with colum_15:
+        plot_gauge(val15, "#1C83E1", "", "PARCELA 3", 8)
+    
+    with colum_16:
+        plot_gauge(val16, "#1C83E1", "", "PARCELA 4", 8)
+
+    with colum_17:
+        plot_gauge(val17, "#1C83E1", "", "PARCELA 5", 8)
+    
+    with colum_18:
+        plot_gauge(val18, "#1C83E1", "", "PARCELA 6", 8)
+
+    with colum_19:
+        plot_gauge(val19, "#1C83E1", "", "PARCELA 7", 8)
+    
+    with colum_20:
+        plot_gauge(val20, "#1C83E1", "", "PARCELA 8", 8)
+
+    with colum_21:
+        plot_gauge(val21, "#1C83E1", "", "PARCELA 9", 8)
+
+    with colum_22:
+        plot_gauge(val22, "#1C83E1", "", "PARCELA 10", 8)
+
+    with colum_23:
+        plot_gauge(val23, "#1C83E1", "", "PARCELA 11", 8)
+
+    with colum_24:
+        plot_gauge(val24, "#1C83E1", "", "PARCELA 12", 8)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
