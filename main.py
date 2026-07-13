00702f009d1e9f68c347f356cf79f1f6e232fbac
João Gabriel <91282960+COOSMOS42@users.noreply.github.com>
2026-07-13 13:24:34 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,7 @@
 import streamlit as st
 
 pg = st.navigation([
-    st.Page("resumo.py", title="Resumo dos Documentos", icon="💽"),
-#    st.Page("glosas.py", title="Glosas", icon="✂️"),
-    st.Page("controlador.py", title="Atualizar Status", icon="💾"),
-    st.Page("consultas.py", title="Consultar Documentos", icon="🗃️"),
-    st.Page("remover.py", title="Remover Documentos", icon="🗑️")
+    st.Page("solit.py", title="Solicitar Peças"),
+    st.Page("consultas.py", title="Consultar Status")
 ])
 pg.run()
