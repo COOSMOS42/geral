a991c869ccb1a703270ca8933ae97fdcd2b896f4
João Gabriel <91282960+COOSMOS42@users.noreply.github.com>
2026-07-13 14:29:06 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import streamlit as st
 
 pg = st.navigation([
+    st.Page("controlador.py", title="Lançamentos"),
     st.Page("edit.py", title="Editar Peças"),
     st.Page("remover.py", title="Remover Peças")
 ])
